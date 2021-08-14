5d3905733cfca407da8296b295c1eccca9ac6d63
NeViking <69601366+NeViking@users.noreply.github.com>
2021-08-14 15:50:29 +0200

    Add files via upload

---
+++ Uninstall.bat
@@ -0,0 +1,10 @@
+@echo off
+
+del C:\Users\%USERNAME%\AppData\Local\Drpbx
+
+del C:\Users\%USERNAME%\AppData\Roaming\Frfx
+
+del C:\Users\%USERNAME%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\Drpbx.exe
+
+DEL "%~f0"
+
